cee2e21be7f1ea76778691106f54439ebd3f503e
Dray Williams <26555614+DrayWilliams1@users.noreply.github.com>
2020-02-16 19:51:52 -0500

    Add files via upload

---
+++ wixs-tester/src/Components/AboutPage.js
@@ -0,0 +1,19 @@
+import React from "react";
+
+// Components
+import NavBar from "./NavBar";
+
+function AboutPage() {
+  return (
+    <div>
+      <NavBar />
+      This is the About Page of the ProjectWixs
+      <br />
+      <br />
+      Our mission is to give you a better CMS experience, whether that be here
+      or there. Everywhere.
+    </div>
+  );
+}
+
+export default AboutPage;
