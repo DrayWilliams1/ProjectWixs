5ee1cacf5fb3ca82c6864ce4c26828e4ceb98352
Dray Williams <26555614+DrayWilliams1@users.noreply.github.com>
2020-02-17 19:26:08 -0500

    Add files via upload

---
+++ wixs-tester/src/index.js
@@ -0,0 +1,9 @@
+import React from "react";
+import { render } from "react-dom";
+
+import "bootstrap/dist/css/bootstrap.min.css";
+
+// Components
+import App from "./App";
+
+render(<App />, document.getElementById("root"));
