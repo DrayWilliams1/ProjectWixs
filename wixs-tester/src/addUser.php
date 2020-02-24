73c6c7631b14ac53bce0c1d4c13750975f4ccbef
Dray Williams <26555614+DrayWilliams1@users.noreply.github.com>
2020-02-23 20:50:59 -0500

    Add files via upload

---
+++ wixs-tester/src/addUser.php
@@ -0,0 +1,19 @@
+#!/usr/bin/php-cgi
+<?php
+ 
+require_once 'dbconfig.php';
+ 
+$dsn = "pgsql:host=$host;port=$port;dbname=$db;user=$username;password=$password";
+ 
+try{
+ // create a PostgreSQL database connection
+ $conn = new PDO($dsn);
+ 
+ // display a message if connected to the PostgreSQL successfully
+ if($conn){
+ echo "Connected to the <strong>$db</strong> database successfully!";
+ }
+}catch (PDOException $e){
+ // report error message
+ echo $e->getMessage();
+}
---
+++ wixs-tester/src/dbConfig.php
@@ -0,0 +1,8 @@
+#!/usr/bin/php-cgi
+<?php
+$host='sandcastle.cosc.brocku.ca';
+$db = 'c4f00g02';
+$port = '5432';
+$username = 'c4f00g02';
+$password = 'A9b3f(Z2V$';
+?>
