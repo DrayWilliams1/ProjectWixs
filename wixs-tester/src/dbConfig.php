fc11baa5b8ed112f767adb6d6fbbb0b53ababeca
Dray Williams <26555614+DrayWilliams1@users.noreply.github.com>
2020-02-28 19:59:24 -0500

    Add files via upload

--- wixs-tester/src/dbConfig.php
+++ wixs-tester/src/dbConfig.php
@@ -1,5 +1,6 @@
 #!/usr/bin/php-cgi
 <?php
+/** A file containing the necessary credentials in order to login to the ProjectWixs Postgres database */
 $host='sandcastle.cosc.brocku.ca';
 $db = 'c4f00g02';
 $port = '5432';
