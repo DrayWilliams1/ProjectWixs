a68d1b4fbe34c237514a5d7527ea39c53234d075
Cameron <camhammel@gmail.com>
2020-03-09 21:43:15 -0400

    +Placeholder templates in dashboard
    Added three placeholder templates in the dashboard (each with their own button themes - would like some input on which looks best!)

--- wixs-tester/src/Components/DashboardPage.js
+++ wixs-tester/src/Components/DashboardPage.js
@@ -1,10 +1,8 @@
 // Dependencies
 import React, { Component } from "react";
-import { Container, Button, Card } from "react-bootstrap";
+import { Container, Button, Card, CardDeck, CardImg, } from "react-bootstrap";
 import axios from 'axios';
-
 import auth from "/auth.js";
-
 
 // CSS/SASS
 import "./sass/DashboardPage.scss";
@@ -41,6 +39,8 @@ export default class DashboardPage extends Component {
   }
 
   render() {
+    //React.createElement(<Cards2/>);
+    //ReactDOM.render(<Cards2/>, document.getElementById("root"));
     const isAuthenticated = auth.isAuthenticated();
     var currentUser = auth.getCookie("user");
     let greeting;
@@ -56,13 +56,68 @@ export default class DashboardPage extends Component {
           <div className="word-content">
             {greeting}
           </div>
-
         </Container>
+
         <Container>
           <div className="template-selection">
             <a href="#/editor"><h2 className="editor-link" >Your Templates</h2></a>
+            <CardDeck>
+              <Card>
+                <Card.Img variant="top"/>
+                <Card.Body>
+                  <Card.Title>Template 1</Card.Title>
+                  <Card.Text>
+                    Placeholder.
+                  </Card.Text>
+                </Card.Body>
+                <Card.Footer>
+                  <small className="text-muted">Last updated 3 mins ago</small>
+                  <span>&nbsp;&nbsp;</span>
+                  <Button variant="primary" size="sm"> Edit </Button>
+                  <span>&nbsp;&nbsp;</span>
+                  <Button variant="danger" size="sm"> Delete </Button>
+                </Card.Footer>
+              </Card>
+              <Card>
+                <Card.Img variant="top"/>
+                <Card.Body>
+                  <Card.Title>Template 2</Card.Title>
+                  <Card.Text>
+                    Placeholder 2.
+                  </Card.Text>
+                </Card.Body>
+                <Card.Footer>
+                  <small className="text-muted">Last updated 3 mins ago</small>
+                  <span>&nbsp;&nbsp;</span>
+                  <Button variant="secondary" size="sm"> Edit </Button>
+                  <span>&nbsp;&nbsp;</span>
+                  <Button variant="danger" size="sm"> Delete </Button>
+                </Card.Footer>
+              </Card>
+              <Card>
+                <Card.Img variant="top"/>
+                <Card.Body>
+                  <Card.Title>Template 3</Card.Title>
+                  <Card.Text>
+                    Placeholder 3 with extra text: This is a wider card with supporting text below as a natural lead-in to
+                    additional content. This card has even longer content than the first to
+                    show that equal height action.
+                  </Card.Text>
+                </Card.Body>
+                <Card.Footer>
+                  <small className="text-muted">Last updated 3 mins ago</small>
+                  <span>&nbsp;&nbsp;</span>
+                  <Button variant="dark" size="sm"> Edit </Button>
+                  <span>&nbsp;&nbsp;</span>
+                  <Button variant="danger" size="sm"> Delete </Button>
+                </Card.Footer>
+              </Card>
+            </CardDeck>
           </div>
         </Container>
+
+        
+
         <Container>
           <div className="gallery-selection">
             <h2>Your Gallery</h2>
