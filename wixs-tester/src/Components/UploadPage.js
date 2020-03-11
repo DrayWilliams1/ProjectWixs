9af77bf55a335301ec472f2fdaa011e23c533cf7
Dray Williams <26555614+DrayWilliams1@users.noreply.github.com>
2020-03-11 16:52:50 -0400

    Basic file uploading
    - Can upload files from 'Upload' page on website and store them in 'uploads' directory within sandcastle
    - Next step: work to limit files to images and videos of specific format. Also to get content recorded within database

--- wixs-tester/src/Components/UploadPage.js
+++ wixs-tester/src/Components/UploadPage.js
@@ -7,6 +7,9 @@ import auth from "/auth.js";
 // CSS/SASS
 import "./sass/UploadPage.scss";
 
+const FILE_UPLOAD_URL =
+  "http://cosc.brocku.ca/~c4f00g02/projectWixs/fileUpload.php";
+
 /**
  * Purpose: This is a file containing...
  */
@@ -19,39 +22,72 @@ export default class UploadPage extends Component {
       first_name: "",
       selectedFile: null
     };
-    this.onChangeHandler = this.onChangeHandler.bind(this);
-    this.onClickHandler = this.onClickHandler.bind(this);
+
+    this.onChange = this.onChange.bind(this);
+    //this.onClickHandler = this.onClickHandler.bind(this);
+    this.onSubmit = this.onSubmit.bind(this);
+    this.uploadFile = this.uploadFile.bind(this);
   }
 
-  //For holding on to the file uploaded in a state
-  onChangeHandler(event) {
+  /**
+   *
+   *
+   * @param {*} event the event that launched the function
+   */
+  async onSubmit(event) {
     event.preventDefault();
-    console.log(event.target.files[0]);
+    let res = await this.uploadFile(this.state.selectedFile);
+
+    console.log(res.data);
+  }
+
+  /**
+   * For holding on to the file uploaded in a state
+   *
+   * @param {*} event the event that launched the function
+   */
+  onChange(event) {
     this.setState({
-      selectedFile: event.target.files[0],
-      loaded: 0
+      selectedFile: event.target.files[0]
     });
   }
 
-  //For saving the file to the server
-  onClickHandler(event) {
-    const data = new FormData();
-    data.append("file", this.state.selectedFile);
-    //axios.post("/", data); //Need PHP Code
+  /**
+   * For saving the file to the server
+   *
+   * @param {*} file
+   */
+  async uploadFile(file) {
+    const formData = new FormData();
+
+    formData.append("avatar", file);
+
+    return await axios
+      .post(FILE_UPLOAD_URL, formData, {
+        headers: {
+          "content-type": "multipart/form-data"
+        }
+      })
+      .then(response => {
+        console.log(response);
+      })
+      .catch(error => {
+        console.log(error);
+      });
   }
 
   render() {
     return (
       <div>
-        <h1>Upload Page</h1>
+        <h1>File Upload Page</h1>
         <h2>Upload your files to the Project Wixs system</h2>
 
         <Container>
           <div className="gallery-selection">
             <h2>Your Gallery</h2>
-            <form>
-              <input type="file" name="file" onChange={this.onChangeHandler} />
-              <Button variant="success" onClick={this.onClickHandler}>
+            <form onSubmit={this.onSubmit}>
+              <input type="file" onChange={this.onChange} />
+              <Button type="submit" variant="success">
                 Upload
               </Button>
             </form>
