77fa90d45c69fa3984da5a7b26039b04b7dc5ac7
Dray Williams <26555614+DrayWilliams1@users.noreply.github.com>
2020-03-11 15:19:20 -0400

    Update fileUpload.php

--- wixs-tester/src/Backend/fileUpload.php
+++ wixs-tester/src/Backend/fileUpload.php
@@ -1,96 +1,58 @@
 #!/usr/bin/php-cgi
-<?php
+<?php 
 /** 
- * A file for uploading files to the ProjectWixs system database
- * 
- * -- Additional Notes --
- * - Do not use any upper case for column names or table names within database (will cause error when 
- * binding PDO values)
+ * A file for uploading files to the sandcastle server
  */
+header('Content-Type: application/json; charset=utf-8');
 header("Access-Control-Allow-Origin: *");
+header("Access-Control-Allow-Methods: PUT, GET, POST");
 
-require_once 'dbConfig.php';
+$response = array();
+$upload_dir = 'uploads/';
+$server_url = 'http://cosc.brocku.ca/~c4f00g02/projectWixs';
 
-$dsn = "pgsql:host=$host;port=$port;dbname=$db;user=$username;password=$password";
+if($_FILES['avatar'])
+{
+    $avatar_name = $_FILES["avatar"]["name"];
+    $avatar_tmp_name = $_FILES["avatar"]["tmp_name"];
+    $error = $_FILES["avatar"]["error"];
 
-$responseObject = array();
-$responseObject['success']=false; // whether the operation executed successfully
-$responseObject['message']=""; // the message from the execution, error or success
+    if($error > 0){
+        $response = array(
+            "status" => "error",
+            "error" => true,
+            "message" => "Error uploading the file!"
+        );
+    }else 
+    {
+        $random_name = rand(1000,1000000)."-".$avatar_name;
+        $upload_name = $upload_dir.strtolower($random_name);
+        $upload_name = preg_replace('/\s+/', '-', $upload_name);
 
-try {
-    // create a PostgreSQL database connection
-    $pdo = new PDO($dsn);
-    
-    // display a message if connected to the PostgreSQL successfully
-       if($pdo) { // connected successfully
-           if (($_SERVER['REQUEST_METHOD'] == 'POST')) {
-               // Using empty test instead of isset function
-               $email_post = empty($_POST['email']) ? null : $_POST['email']; // set email to form submission
-   
-               if (validInputs()) {
-                   $responseObject['success']=true; // echoing a response that can be used to redirect page after AJAX call
-               } // otherwise, error, response message is displayed in alert
-               
-           } else { // request method is not POST
-               $responseObject['message']="Invalid request. ";
-           }
-       }
-   } catch (PDOException $e) { 
-       $responseObject['message']=$e->getMessage(); // report error message
-   }
+        if(move_uploaded_file($avatar_tmp_name , $upload_name)) {
+            $response = array(
+                "status" => "success",
+                "error" => false,
+                "message" => "File uploaded successfully",
+                "url" => $server_url."/".$upload_name
+              );
+        }else
+        {
+            $response = array(
+                "status" => "error",
+                "error" => true,
+                "message" => "Error uploading the file!"
+            );
+        }
+    }    
 
-/**
- * First checks if any inputs are missing, then validates and sanitizes certain inputs. 4 Rounds of validation.
- * 
- * @return boolean true if no inputs are mssing, and all inputs are valid. Then sanitizes them. False, if any of those
- * criteria are not met.
- * */
-function validInputs() {
-    global $email_post, $password_post, $sessionID_post;
-    global $responseObject;
-
-    // Round 1 -- Are inputs empty
-    // does not check template count because field may be null
-    if(empty($email_post)) {
-        $responseObject['message']="One of more fields are missing. Please complete. ";
-        return false; // invalid
-    }
-
-
-    // checking if valid user email provided
-    if(!filter_var($email_post, FILTER_VALIDATE_EMAIL)){
-        $responseObject['message']="Invalid email: formatting.  ";
-        return false; // invalid
-    }
-
-    // Round 4 -- Sanitize inputs
-    // if no inputs are missing or invalid, they are sanitized.
-    $email_post = filter_var($email_post,FILTER_SANITIZE_EMAIL);
-
-    return true; // tests passed -> valid
+}else{
+    $response = array(
+        "status" => "error",
+        "error" => true,
+        "message" => "No file was sent!"
+    );
 }
 
-/**
- * 
- */
-function fileUpload() {
-    global $pdo;
-    global $email_post;
-    global $responseObject;
-
-    $sql_select = "SELECT * FROM templates WHERE email = ?";
-    $stmt = $pdo->prepare($sql_select);
-
-    // pass and bind values to the statement
-    $stmt->bindValue(1, $email_post, PDO::PARAM_STR); // binding to string
-
-    if($stmt->execute()) { // The query has executed successfully
-        // we'll know we have the templates
-
-    } else {
-        $responseObject['message']="Error querying users table. ";
-    }
-}
-
-echo json_encode($responseObject);
+echo json_encode($response);
 ?>
