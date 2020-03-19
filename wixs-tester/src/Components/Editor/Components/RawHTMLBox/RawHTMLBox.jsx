fa654bdbf826f248fce28ffefcc411be408bf040
GitG0 <gmet757@gmail.com>
2020-03-18 21:23:40 -0400

    Created html box skeleton
    base for a raw html editor box

---
+++ wixs-tester/src/Components/Editor/Components/RawHTMLBox/RawHTMLBox.jsx
@@ -0,0 +1,31 @@
+import React, {Component} from 'react';
+import "./RawHTMLBox.scss";
+
+export class RawHTMLBox extends Component {
+
+  render() {
+    return (
+      <div {...this.props} className={[this.props.className, "rawHTMLBox-style"].join(' ')}>
+        {this.props.children}
+        <body>
+            {this.props.content.value}
+        </body>
+      </div>
+    );
+  }
+}
+
+// legend information
+export const SCHEMA = {
+  type: RawHTMLBox,
+  gridOptions: {h: 2, w: 2, minW: 2, minH: 2},     // grid options parameters: minW, maxW, minH, maxH, isDraggable, isResizable, static
+  title: "HTML box",
+  desc: "A box to allow for raw html code",
+  props: {
+    content: {
+      type: "StringArea",
+      name: "Content",
+      value: "Code goes here",
+    }
+  }
+};
--- wixs-tester/src/Components/Editor/EDITOR_CONSTANTS.js
+++ wixs-tester/src/Components/Editor/EDITOR_CONSTANTS.js
@@ -2,11 +2,13 @@ import {SCHEMA as TextboxSchema} from "./Components/textbox/textbox";
 import {SCHEMA as RichTexboxSchema} from "./Components/richTextbox/RichTextBox";
 import {SCHEMA as ContentWithHeaderSchema} from "./Components/ContentWithHeader/ContentWithHeader";
 import {SCHEMA as StandardButtonSchema} from "./Components/StandardButton/StandardButton";
+import {SCHEMA as RawHTMLBoxSchema} from "./Components/RawHTMLBox/RawHTMLBox";
 
 
 export const LEGEND = {
   Textbox: TextboxSchema,
   RichTextbox: RichTexboxSchema,
   ContentWithHeader: ContentWithHeaderSchema,
-  StandardButton: StandardButtonSchema
+  StandardButton: StandardButtonSchema,
+  RawHTMLBox: RawHTMLBoxSchema
 };
