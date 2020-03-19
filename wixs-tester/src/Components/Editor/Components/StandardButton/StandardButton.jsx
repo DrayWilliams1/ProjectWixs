6bd0d6722dac6a33d4df31873f5311ac7a29dde4
GitG0 <gmet757@gmail.com>
2020-03-18 21:08:00 -0400

    Modified button component
    Changed name to prevent mixup with other elements

--- wixs-tester/src/Components/Editor/Components/StandardButton/StandardButton.jsx
+++ wixs-tester/src/Components/Editor/Components/StandardButton/StandardButton.jsx
@@ -1,11 +1,11 @@
 import React, {Component} from 'react';
-import "./button.scss";
+import "./StandardButton.scss";
 
-export class Button extends Component {
+export class StandardButton extends Component {
 
   render() {
     return (
-      <div {...this.props} className={[this.props.className, "button-style"].join(' ')}>
+      <div {...this.props} className={[this.props.className, "StandardButton-style"].join(' ')}>
         {this.props.children}
         <button>{this.props.content.value}</button>
       </div>
@@ -15,7 +15,7 @@ export class Button extends Component {
 
 // legend information
 export const SCHEMA = {
-  type: Button,
+  type: StandardButton,
   gridOptions: {h: 2, w: 2, minW: 2, minH: 2},     // grid options parameters: minW, maxW, minH, maxH, isDraggable, isResizable, static
   title: "Button",
   desc: "A Standard Button",
--- wixs-tester/src/Components/Editor/EDITOR_CONSTANTS.js
+++ wixs-tester/src/Components/Editor/EDITOR_CONSTANTS.js
@@ -1,13 +1,12 @@
 import {SCHEMA as TextboxSchema} from "./Components/textbox/textbox";
 import {SCHEMA as RichTexboxSchema} from "./Components/richTextbox/RichTextBox";
 import {SCHEMA as ContentWithHeaderSchema} from "./Components/ContentWithHeader/ContentWithHeader";
-import {SCHEMA as ButtonSchema} from "./Components/button/button";
+import {SCHEMA as StandardButtonSchema} from "./Components/StandardButton/StandardButton";
 
 
 export const LEGEND = {
   Textbox: TextboxSchema,
   RichTextbox: RichTexboxSchema,
   ContentWithHeader: ContentWithHeaderSchema,
-  Button: ButtonSchema
- 
+  StandardButton: StandardButtonSchema
 };
