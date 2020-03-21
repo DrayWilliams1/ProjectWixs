4a9097dcbd64ff13754f92588dbc97c804f85737
Dray Williams <26555614+DrayWilliams1@users.noreply.github.com>
2020-03-21 11:46:18 -0400

    File deletion added
    - Files are deleted from database when delete button is clicked

--- wixs-tester/src/Components/CustomMedia.js
+++ wixs-tester/src/Components/CustomMedia.js
@@ -1,13 +1,18 @@
 // Dependencies
 import React, { Component } from "react";
-import {Card, Button} from 'react-bootstrap';
+import { Card, Button } from "react-bootstrap";
+import axios from "axios";
+import qs from "qs"; // for packaging details collected from the form
 
 // CSS/SASS
 import "./sass/CustomMedia.scss";
 
+const DELETE_MEDIA_URL =
+  "http://cosc.brocku.ca/~c4f00g02/projectWixs/deleteMedia.php";
+
 /**
  * Purpose: This is a file displaying a photo or video element depending on the props sent to it
- * 
+ *
  * -- Additional Notes --
  * - In React it is suggested to avoid setting state to equal props in the constructor. Just access props directly
  * as this will help keep track when updating whther props changed
@@ -20,30 +25,42 @@ export default class MediaComponent extends Component {
     const baseType = type.substring(0, type.indexOf("/")); // helps identify general file type (image, video, etc.)
 
     this.state = {
-      file_type: baseType,
+      file_type: baseType
     };
 
     this.deleteContent = this.deleteContent.bind(this);
   }
 
   /**
-   * 
-   * 
-   * @param {*} event 
+   *
+   *
+   * @param {*} event
    */
   deleteContent(event) {
     event.preventDefault();
 
-    if(this.props.content['file_name']) { // If the file name data exists
+    if (this.props.content["content_id"]) {
+      // If the file name data exists
 
-      const params = { // Only need to send file name as it is unique
-        file_name: this.props.content['file_name']
+      const params = {
+        // Only need to send file name as it is unique
+        content_id: this.props.content["content_id"]
       };
 
-      console.log("Delete button clicked for " + this.props.content['file_name']);
+      axios
+        .post(DELETE_MEDIA_URL, qs.stringify(params))
+        .then(response => {
+          console.log(response);
 
+          alert(response.data["message"]);
+          window.location.reload();
+        })
+        .catch(error => {
+          console.log(error);
+        });
+      //console.log("Delete button clicked for " + this.props.content['file_name']);
     } else {
-      alert("Error capturing file data for submission");
+      alert("Error capturing file data for deletion");
     }
   }
 
@@ -52,12 +69,24 @@ export default class MediaComponent extends Component {
       // an image
       return (
         <Card>
-          <Card.Img className="image-content" variant="top" src={this.props.content['file_location']}/>
+          <Card.Img
+            className="image-content"
+            variant="top"
+            src={this.props.content["file_location"]}
+          />
           <Card.Title>Image {this.props.listNum}</Card.Title>
-          <Card.Text><strong>File name: </strong>{this.props.content['file_name']}</Card.Text>
-          <Button className="content-delete-button" variant="danger" onClick={this.deleteContent}>Delete</Button>
+          <Card.Text>
+            <strong>File name: </strong>
+            {this.props.content["file_name"]}
+          </Card.Text>
+          <Button
+            className="content-delete-button"
+            variant="danger"
+            onClick={this.deleteContent}
+          >
+            Delete
+          </Button>
         </Card>
-        
       );
     } else {
       // a video
