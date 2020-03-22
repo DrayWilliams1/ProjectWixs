cc1b8ac72735a674793e236eb0e3bda4c8599a3a
Dray Williams <26555614+DrayWilliams1@users.noreply.github.com>
2020-03-22 19:05:32 -0400

    Fixed Dashboard Edit/Delete buttons
    - Templates can now be deleted from database
    - Edit button will redirect to the editor page. Will eventually include template_id in URL so template can be fetched and loaded on editor page.

--- wixs-tester/src/Components/CustomCard.js
+++ wixs-tester/src/Components/CustomCard.js
@@ -1,10 +1,13 @@
 // Dependencies
 import React, { Component } from "react";
-import { Container, Button, Card, CardDeck, CardImg } from "react-bootstrap";
+import { Button, Card } from "react-bootstrap";
+import axios from "axios";
+import qs from "qs"; // for packaging details collected from the form
 
 // CSS/SASS
 import "./sass/CustomCard.scss";
-const DEL_TEMPLATE_URL = "http://cosc.brocku.ca/~c4f00g02/projectWixs/deleteTemplate.php";
+const DEL_TEMPLATE_URL =
+  "http://cosc.brocku.ca/~c4f00g02/projectWixs/deleteTemplate.php";
 
 /**
  * Purpose: This is a file displaying a card element to represent a template on the dashboard page
@@ -14,6 +17,8 @@ export default class CustomCard extends Component {
     super(props);
 
     this.dateDifference = this.dateDifference.bind(this);
+    this.toEditor = this.toEditor.bind(this);
+    this.delTemplate = this.delTemplate.bind(this);
   }
 
   /**
@@ -45,90 +50,87 @@ export default class CustomCard extends Component {
     return total;
   }
 
+  /**
+   * Directs to the template editor page while including the template to be loaded and edited
+   */
   toEditor() {
-    window.location.href="#/editor";
-}
+    window.location.href = "#/editor";
+    // TODO: will eventually include the template_id in the url so it can be retrieved by the editor page. Can append it -- will discuss
+  }
 
-    /**
+  /**
    * Deletes a specified user template from the database
    */
   delTemplate() {
     const params = {
-      temp_id: this.props.template["template_id"]
+      template_id: this.props.template["template_id"]
     };
 
     axios
       .post(DEL_TEMPLATE_URL, qs.stringify(params))
       .then(response => {
         console.log(response);
-        if (response.data["success"] === true) {
-          console.log("template deleted.");
-          this.setState({
-            templateArray: response.data["templates"]
-          });
-        } else {
-          console.log(response.data["message"]);
-        }
+
+        alert(response.data["message"]);
+        window.location.reload();
       })
       .catch(error => {
         console.log(error);
       });
   }
 
-
   render() {
-            if (this.props.template['is_active'])
-            {
-                return (
-                <Card border="success">
-                  <Card.Body>
-                    <Card.Title>{this.props.template['custom_name']}
-                    <span>&nbsp;&nbsp;</span>
-                    <small className="text-muted">  (Active Template) </small>
-                    </Card.Title>
-                    <Card.Text>
-                    
-                    <Button variant="primary" size="sm" onClick="toEditor()">
-                      {" "}
-                      Edit{" "}
-                    </Button>
-                    <span>&nbsp;&nbsp;</span>
-                    <Button variant="danger" size="sm" onClick="delTemplate()">
-                      {" "}
-                      Delete{" "}
-                    </Button>
-                    </Card.Text>
-                  </Card.Body>
-                  <Card.Footer>
-                    <small className="text-muted">
-                      Last updated {this.dateDifference(this.props.template['last_modified'])}
-                    </small>
-                    <span>&nbsp;&nbsp;</span>
-                  </Card.Footer>
-                </Card>
-                );
-            } else {
-                return (
-                <Card>
-                <Card.Body>
-                  <Card.Title>{this.props.template['custom_name']}</Card.Title>
-                  <Card.Text> 
-                    <Button variant="primary" size="sm">
-                      {" "}
-                      Edit{" "}
-                    </Button>
-                    <span>&nbsp;&nbsp;</span>
-                    <Button variant="danger" size="sm" onClick="delTemplate()">
-                      {" "}
-                      Delete{" "}
-                    </Button></Card.Text>
-                </Card.Body>
-                <Card.Footer>
-                  <small className="text-muted">
-                    Last updated {this.dateDifference(this.props.template['last_modified'])}
-                  </small>
-                </Card.Footer>
-              </Card>
-            )}
+    if (this.props.template["is_active"]) {
+      return (
+        <Card border="success">
+          <Card.Body>
+            <Card.Title>
+              {this.props.template["custom_name"]}
+              <span>&nbsp;&nbsp;</span>
+              <small className="text-muted"> (Active Template) </small>
+            </Card.Title>
+            <Card.Text>
+              <Button variant="primary" size="sm" onClick={this.toEditor}>
+                Edit
+              </Button>
+              <span>&nbsp;&nbsp;</span>
+              <Button variant="danger" size="sm" onClick={this.delTemplate}>
+                Delete
+              </Button>
+            </Card.Text>
+          </Card.Body>
+          <Card.Footer>
+            <small className="text-muted">
+              Last updated{" "}
+              {this.dateDifference(this.props.template["last_modified"])}
+            </small>
+            <span>&nbsp;&nbsp;</span>
+          </Card.Footer>
+        </Card>
+      );
+    } else {
+      return (
+        <Card>
+          <Card.Body>
+            <Card.Title>{this.props.template["custom_name"]}</Card.Title>
+            <Card.Text>
+              <Button variant="primary" size="sm" onClick={this.toEditor}>
+                Edit
+              </Button>
+              <span>&nbsp;&nbsp;</span>
+              <Button variant="danger" size="sm" onClick={this.delTemplate}>
+                Delete
+              </Button>
+            </Card.Text>
+          </Card.Body>
+          <Card.Footer>
+            <small className="text-muted">
+              Last updated
+              {this.dateDifference(this.props.template["last_modified"])}
+            </small>
+          </Card.Footer>
+        </Card>
+      );
+    }
   }
 }
